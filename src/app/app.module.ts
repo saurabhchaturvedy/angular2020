7e5a092ff2c968df11e1a44a476adfcb817c24b5
Saurabh Chaturvedi <>
2019-10-27 00:50:04 +0530

    [hithead-2020] commiting initial angular app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
